3fb53637a2bddc05ddda1ddc1162cbd2d52910fe
tliebert1 <77161652+tliebert1@users.noreply.github.com>
2023-06-12 12:08:56 -0600

    .env file, baseURL, realative paths

--- ProjectOnePHP/app/Config/App.php
+++ ProjectOnePHP/app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://localhost:8080/';
+    public $baseURL = 'https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/';
 
     /**
      * --------------------------------------------------------------------------
