86f2cdec8d1e0ebcc2d2241c118ee2b2b2b9bc45
Mike Stratton <134551281+mstratton1@users.noreply.github.com>
2023-06-14 10:24:53 -0400

    updated url for footer

--- ProjectOnePHP/app/Views/includes/footer.php
+++ ProjectOnePHP/app/Views/includes/footer.php
@@ -1,3 +1,3 @@
-<script src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/uswds/js/uswds.min.js"></script>
+<script src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/uswds/uswds/js/uswds.min.js"></script>
     </body>
 </html>
