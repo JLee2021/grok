3cdda40bf6c0a648508013c1d3fc8d7b83d0f9fc
Mike Stratton <134551281+mstratton1@users.noreply.github.com>
2023-06-16 11:55:52 -0400

    new-haul form working

--- ProjectOnePHP/app/Views/new-trip.php
+++ ProjectOnePHP/app/Views/new-trip.php
@@ -1,37 +1,18 @@
 <section id="test-section-id" class="usa-section">
-<div class="grid-container">
-    <div class="mobile-lg:grid-col-4 margin-top-4 mobile-lg:margin-top-0">
-        <form class="usa-form" id="new_trip" onsubmit="event.preventDefault();">
-                    <fieldset class="usa-fieldset">
+    <div class="grid-container">
+        <div class="mobile-lg:grid-col-4 margin-top-4 mobile-lg:margin-top-0">
+            <form class="usa-form" id="new_trip" onsubmit="event.preventDefault();">
+                <fieldset class="usa-fieldset">
                     <legend class="usa-legend usa-legend--large">Atlas Data Entry</legend>
                     <label class="usa-label" for="obisd">Observer ID</label>
-                    <input
-                        class="usa-input"
-                        id="obsid"
-                        name="obsid"
-                        type="text"
-                        autocapitalize="off"
-                        autocorrect="off"
-                        required
-                    />
+                    <input class="usa-input" id="obsid" name="obsid" type="text" autocapitalize="off" autocorrect="off" required />
                     <label class="usa-label" for="obisd">Trip Number</label>
-                    <input
-                        class="usa-input"
-                        id="trip_number"
-                        name="trip_number"
-                        type="text"
-                        autocapitalize="off"
-                        autocorrect="off"
-                        required
-                    />
-                    
+                    <input class="usa-input" id="trip_number" name="trip_number" type="text" autocapitalize="off" autocorrect="off" required />
+
                     <input class="usa-button" type="submit" value="Start Trip" />
-                    
-                    </fieldset>
-                </form>
 
-
-           
+                </fieldset>
+            </form>
         </div>
     </div>
 </section>
@@ -47,16 +28,17 @@
     
         if (observerid.value == "" || tripNumber.value == "") {
         alert("Ensure you input a value in both fields!");
-        } else {
-        // perform operation with form input
-        alert("This form has been successfully submitted!");
-        console.log(
-            `This form has a Observer ID of ${observerid.value} and Trip Number of ${tripNumber.value}`
-        );
-    
-        observerid.value = "";
-        tripNumber.value = "";
-    }
-});
+        } 
+        else {
+            // perform operation with form input
+            alert("This form has been successfully submitted!");
+            console.log(
+                `This form has a Observer ID of ${observerid.value} and Trip Number of ${tripNumber.value}`
+            );
+        
+            observerid.value = "";
+            tripNumber.value = "";
+        }
+    });
 
 </script>
