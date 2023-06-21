93a7d8a7f2d5c1cd47755d87d9a66e9b1fb268ab
Mike Stratton <134551281+mstratton1@users.noreply.github.com>
2023-06-21 03:48:42 -0400

    new catch form pulling from api and saving data in console via javascript

--- ProjectOnePHP/app/Controllers/Home.php
+++ ProjectOnePHP/app/Controllers/Home.php
@@ -4,55 +4,55 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index()
-    {
-        return view('welcome_message');
-    }
+  public function index()
+  {
+    return view('welcome_message');
+  }
 
-    public function test($fieldname)
-    {
-      $dropdown = new \App\Models\DropdownModel();
-      $this->session = \Config\Services::session();
-      $token = $this->session->token;
-      $result = $dropdown->get_values($fieldname, $token);
-      $this->response->setHeader('Content-type', 'application/json');
-      echo json_encode($result);
-    }
+  public function test($fieldname)
+  {
+    $dropdown = new \App\Models\DropdownModel();
+    $this->session = \Config\Services::session();
+    $token = $this->session->token;
+    $result = $dropdown->get_values($fieldname, $token);
+    $this->response->setHeader('Content-type', 'application/json');
+    echo json_encode($result);
+  }
 
-    public function login()
-    {
-      $this->session = \Config\Services::session();
-      $data = array('auth_msg' => $this->session->auth_msg);
-      $this->session->destroy();
+  public function login()
+  {
+    $this->session = \Config\Services::session();
+    $data = array('auth_msg' => $this->session->auth_msg);
+    $this->session->destroy();
 
-      return view('includes/header')
-              . view('login', $data)
-              //. view('js/login.js')
-              . view('includes/footer');
-    }
-    private function get_token_script()
-    {
-        $this->session = \Config\Services::session();
-        $token = $this->session->token;
-        return '        const token="'.$token.'";'."\n";
-    }
+    return view('includes/header')
+      . view('login', $data)
+      //. view('js/login.js')
+      . view('includes/footer');
+  }
+  private function get_token_script()
+  {
+    $this->session = \Config\Services::session();
+    $token = $this->session->token;
+    return '        const token="' . $token . '";' . "\n";
+  }
 
-    public function dashboard()
-    {
-      //return view('dashboard-user');
-      return view('includes/header')
-              . view('dashboard-user')
-              . $this->get_token_script()
-              . view('includes/footer');
-    }
+  public function dashboard()
+  {
+    //return view('dashboard-user');
+    return view('includes/header')
+      . view('dashboard-user')
+      . $this->get_token_script()
+      . view('includes/footer');
+  }
 
 
   public function dashboard_trip()
   {
     return view('includes/header')
-            . view('dashboard-trip')
-            // . view('js/main.js')
-            . view('includes/footer');
+      . view('dashboard-trip')
+      // . view('js/main.js')
+      . view('includes/footer');
   }
 
   public function new_trip()
@@ -67,63 +67,71 @@ class Home extends BaseController
       'ports' => $dropdown->get_values('port', $token)
     );
     return view('includes/header')
-            . view('new-trip', $data)
-            . view('includes/footer');
+      . view('new-trip', $data)
+      . view('includes/footer');
   }
 
   public function new_haul()
   {
     return view('includes/header')
-            . view('new-haul')
-            // . view('js/main.js')
-            . view('includes/footer');
+      . view('new-haul')
+      // . view('js/main.js')
+      . view('includes/footer');
   }
 
-    public function new_catch()
+  public function new_catch()
   {
+    $dropdown = new \App\Models\DropdownModel();
+    $this->session = \Config\Services::session();
+    $token = $this->session->token;
+    $data = array(
+      'disposition' => $dropdown->get_values('disposition_code', $token),
+      'species' => $dropdown->get_values('species_itis', $token),
+      'ports' => $dropdown->get_values('port', $token)
+    );
     return view('includes/header')
-            . view('new-catch')
-            // . view('js/main.js')
-            . view('includes/footer');
+      . view('new-catch', $data)
+      // . view('js/main.js')
+      . view('includes/footer');
   }
 
   public function log_catch()
   {
     return view('includes/header')
-            . view('log-catch')
-            // . view('js/main.js')
-            . view('includes/footer');
+      . view('log-catch')
+      // . view('js/main.js')
+      . view('includes/footer');
   }
 
   public function end_haul()
   {
     return view('includes/header')
-            . view('end-haul')
-            // . view('js/main.js')
-            . view('includes/footer');
+      . view('end-haul')
+      // . view('js/main.js')
+      . view('includes/footer');
   }
 
   public function form_links()
   {
     return view('includes/header')
-            . view('form-links')
-            // . view('js/main.js')
-            . view('includes/footer');
+      . view('form-links')
+      // . view('js/main.js')
+      . view('includes/footer');
   }
 
   public function splash()
   {
     return view('includes/header')
-            . view('splash')
-            // . view('js/main.js')
-            . view('includes/footer');
+      . view('splash')
+      // . view('js/main.js')
+      . view('includes/footer');
   }
 
   public function gps()
   {
     return view('includes/header')
-            . view('gps')
-            // . view('js/main.js')
-            . view('includes/footer');
+      . view('gps')
+      // . view('js/main.js')
+      . view('includes/footer');
   }
 }
--- ProjectOnePHP/app/Views/new-trip.php
+++ ProjectOnePHP/app/Views/new-trip.php
@@ -5,42 +5,41 @@
                 <fieldset class="usa-fieldset">
                     <legend class="usa-legend usa-legend--small">New Trip</legend>
                     <legend class="usa-legend usa-legend--large">Atlas Data Entry</legend>
-<!--obsid -->
+                    <!--obsid -->
                     <label class="usa-label" for="obsid">Observer ID</label>
                     <select class="usa-select" name="obsid" id="obsid">
                         <option selected="" disabled>- Select -</option>
-<?php
-    foreach($observer as $obs)
-    {
-        $option  = '                        <option value="'.$obs->value.'" title="'.$obs->name.'"';
-        if($obs->name == $username) { $option .= ' selected' ; };
-        $option .= ' disabled>'.$obs->descr.'</option>';
-        echo $option;
-    }
-?>
+                        <?php
+                        foreach ($observer as $obs) {
+                            $option  = '                        <option value="' . $obs->value . '" title="' . $obs->name . '"';
+                            if ($obs->name == $username) {
+                                $option .= ' selected';
+                            };
+                            $option .= ' disabled>' . $obs->descr . '</option>';
+                            echo $option;
+                        }
+                        ?>
                     </select>
-<!--vessel_permit_num -->
+                    <!--vessel_permit_num -->
                     <label class="usa-label" for="vessel_permit_num">Vessel</label>
                     <select class="usa-select" name="vessel_permit_num" id="vessel_permit_num">
                         <option selected="" disabled>- Select -</option>
-<?php
-    foreach($vessels as $ves)
-    {
-        echo '                        <option value="'.$ves->name.'" title="'.$ves->descr.'">'.$ves->name.'</option>';
-    }
-?>
+                        <?php
+                        foreach ($vessels as $ves) {
+                            echo '                        <option value="' . $ves->name . '" title="' . $ves->descr . '">' . $ves->name . '</option>';
+                        }
+                        ?>
                     </select>
 
-<!--port -->
+                    <!--port -->
                     <label class="usa-label" for="port">Port</label>
                     <select class="usa-select" name="port" id="port" required>
                         <option selected="" disabled>- Select -</option>
-<?php
-    foreach($ports as $port)
-    {
-        echo '                        <option value="'.$port->name.'" title="'.$port->descr.'">'.$port->name.'</option>';
-    }
-?>
+                        <?php
+                        foreach ($ports as $port) {
+                            echo '                        <option value="' . $port->name . '" title="' . $port->descr . '">' . $port->name . '</option>';
+                        }
+                        ?>
                     </select>
 
                     <label class="usa-label" for="trip_number">Trip Number</label>
@@ -64,9 +63,8 @@
         let halt = get_form_data();
 
         if (halt) {
-          alert("Ensure you input a value in both fields!");
-        }
-        else {
+            alert("Ensure you input a value in both fields!");
+        } else {
             // perform operation with form input
             alert("This form has been successfully submitted!");
             console.log(
@@ -76,16 +74,15 @@
     });
 
     function get_form_data() {
-      let form = document.querySelector('#new_trip');
-      let data = new FormData(form);
-      let halt = false;
-      for (let [key, value] of data) {
-          console.log(key, value);
-          if(value.length==0) {
-              halt = true;
+        let form = document.querySelector('#new_trip');
+        let data = new FormData(form);
+        let halt = false;
+        for (let [key, value] of data) {
+            console.log(key, value);
+            if (value.length == 0) {
+                halt = true;
+            }
         }
-      }
-      return halt;
+        return halt;
     }
-
 </script>
