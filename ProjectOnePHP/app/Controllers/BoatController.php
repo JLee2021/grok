0c86e289c0a2642a06fdfb8ff51b5b9a5f4d33b0
Mike Stratton <134551281+mstratton1@users.noreply.github.com>
2023-06-22 12:20:02 -0400

    created and read functionality using .csv file

--- ProjectOnePHP/app/Controllers/BoatController.php
+++ ProjectOnePHP/app/Controllers/BoatController.php
@@ -5,13 +5,21 @@ namespace App\Controllers;
 class BoatController extends BaseController
 {
   public function index()
-  {
+  { 
+    $boat = new \App\Models\BoatModel();
+    $file = "https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/boat/boats.csv";
+    $readfile = $boat->read_file($file); 
+    $data = array(
+      'boats' => $readfile
+    );
+
     $breadcrumbs['nav'] =  array(
       array('name' => 'Home', 'url' => '/'),
       array('name' => 'Boats', 'url' => null)
     );
+
     return view('includes/header', $breadcrumbs)
-      . view('boats/index')
+      . view('boats/index', $data)
       . view('includes/footer');
   }
 
@@ -19,6 +27,7 @@ class BoatController extends BaseController
   {
     $breadcrumbs['nav'] =  array(
       array('name' => 'Home', 'url' => '/'),
+      array('name' => 'All Boats', 'url' => 'BoatController/index'),
       array('name' => 'New Boat', 'url' => null)
     );
     return view('includes/header', $breadcrumbs)
@@ -26,19 +35,31 @@ class BoatController extends BaseController
       . view('includes/footer');
   }
 
-  public function store(){
-    // TODO: Save to JSON via model
+  public function store()
+  {
+    $boat_name = $this->request->getPost(['boat-name']);
+    $implode = implode($boat_name);
+    $name = ',' . $implode;
+    $file = "/var/www/html/grok/html/ProjectOnePHP/public/boat/boats.csv";
+    file_put_contents($file, $name, FILE_APPEND);
+
+    // return $this->index();
+    
+    return redirect()->back();
   }
 
-  public function edit($id){
+  public function edit($id)
+  {
     // TODO: Return view to update
   }
 
-  public function update($id){
+  public function update($id)
+  {
     // TODO: save update to JSON 
   }
 
-  public function delete($id){
+  public function delete($id)
+  {
     // TODO: hard delete from JSON
   }
 }
