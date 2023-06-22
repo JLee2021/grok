7ac8de5c588fa7278f17c68fdf99e0de3f7ef7e6
Tyler Gillispie <tyler.gillispie@noaa.gov>
2023-06-22 12:43:48 -0400

    Add the domain layer for vessels; Add a vessel-list component; Add (vessel) store object to abstract indexDB;

--- ProjectTwoReact/src/counter.js
+++ ProjectTwoReact/src/counter.js
@@ -1,7 +1,17 @@
+import { VesselCtrl } from "./controller/vessel"
+const vslCtrl = new VesselCtrl()
+const vslModel = vslCtrl.getModel()
+
+const vpNoBase = 10000
+
 export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
     counter = count
+    vslModel.store.addOne({
+      name: `Test-${count}`,
+      id: vpNoBase + count
+    })
     element.innerHTML = `count is ${counter}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
