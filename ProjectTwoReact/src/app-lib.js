a6a2436b4743b78d025a8256d621341e01938b30
Tyler Gillispie <tyler.gillispie@noaa.gov>
2023-06-27 11:21:49 -0400

    Add reactive object proxies and watchers to update the component.

---
+++ ProjectTwoReact/src/app-lib.js
@@ -0,0 +1,47 @@
+/* Create a custom proxy object used for (watch)ing object changes.
+ *
+ * @param {object} obj  an object, array, or function: see MDN proxy API.
+ * @param {function} callback  an optional update function when obj changes.
+ * @return ref object  An object with a target and value parameter.
+ */
+export function ref(obj, callback = null) {
+  let intercepts = {}
+  if (typeof callback === 'function' ) {
+    intercepts = {
+      set(target, prop, val, ref) {
+        console.log('tprv: %o, %o, %o, %o', target, prop, val, ref)
+        callback(val, ref[prop] || null);
+        target[prop] = val
+        return true
+      }
+    }
+  }
+
+	return { target: obj, value: new Proxy(obj, intercepts) }
+}
+
+/* Watch for changes in a custom proxy (ref) object.  When ref.value is modified.
+ *
+ * Low level details: Intercepts the "set" action and runs a callback.
+ * Nests the passed in value property (proxy) inside another proxy.
+ *
+ * @param {object} ref  a reference object: see ref function; value prop is a Proxy.
+ * @param {function} callback  the function to call when "set" action is intercepted.
+ *
+ */
+export function watch(ref, callback) {
+	// Wrap the current proxy with a new proxy and callback.
+	ref.value = new Proxy(ref.value, {
+		set(target, prop, val, ref) {
+      // Not sure why this is happening.
+      if (prop == 'length') {
+        return true
+      }
+      console.log('tprv: %o, %o, %o, %o', target, prop, val, ref)
+			callback(val, ref[prop] || null)
+			target[prop] = val
+			return true
+		}
+  })
+}
+
--- ProjectTwoReact/src/counter.js
+++ ProjectTwoReact/src/counter.js
@@ -1,10 +1,20 @@
+import { watch } from "./app-lib"
 import { vesselApi } from "./service/api"
-(async () => {
-  const vessels = await vesselApi.get()
-  console.info('Vessels: %o', vessels)
-})()
+import { vessel } from "./store/vessel"
+
+// Make an API request for vessels.
+vesselApi.get()
 
 export function setupCounter(element) {
+  console.info('setup vessel watcher')
+  watch(vessel, (n, o) => {
+    console.log('Updating Component')
+    console.log('New Value: %o', n)
+    console.log('Old Value: %o', o)
+
+    setCounter(n.length)
+  })
+
   let counter = 0
   const setCounter = (count) => {
     counter = count
