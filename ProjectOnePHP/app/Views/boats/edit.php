c8b06cf90631ad6d5ec4d922c5481bc53d6261b4
Mike Stratton <134551281+mstratton1@users.noreply.github.com>
2023-06-28 01:47:55 -0400

    CRUD app now checks for connection before submitting data to file, hides button if no connection found

--- ProjectOnePHP/app/Views/boats/edit.php
+++ ProjectOnePHP/app/Views/boats/edit.php
@@ -1,8 +1,9 @@
 <section id="test-section-id" class="usa-section">
     <div class="grid-container">
         <div class="mobile-lg:grid-col-8 margin-top-4 mobile-lg:margin-top-0">
-            <h1 class="site-preview-heading margin-0">Update Boats</h1>
-            
+            <h1 class="site-preview-heading margin-0">Update Vessels</h1>
+            <h3>Network Connection: <span id="status"></span></h3>
+            <div id="boats">
              <?php
                 
                 foreach($boats as $boat){
@@ -14,9 +15,41 @@
                     echo "</form>";
                 }
              ?>
-            
+            </div>
             <br>
-            <a href="index"><button type="button" class="usa-button usa-button--base">Cancel</button></a> 
+            <a href="index"><button type="button" class="usa-button usa-button--base" id="button">Cancel</button></a> 
         </div>
     </div>
 </section>
+
+<script>
+const checkOnlineStatus = async () => {
+  try {
+    const online = await fetch("https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/1pixel.png");
+    return online.status >= 200 && online.status < 300; // either true or false
+  } catch (err) {
+    return false; 
+  }
+};
+
+setInterval(async () => {
+  const result = await checkOnlineStatus();
+  const statusDisplay = document.getElementById("status");
+  const buttonDisplay = document.getElementById("button")
+  const boatsDisplay = document.getElementById("boats")
+  statusDisplay.textContent = result ? "Online" : "Offline";
+  statusDisplay.style.color = result ? "#005ea2" : "#d83933";
+  buttonDisplay.style.display = result ? "" : "none";
+  boatsDisplay.style.display = result ? "" : "none";
+}, 3000); // probably too often, try 30000 for every 30 seconds
+
+window.addEventListener("load", async (event) => {
+  const statusDisplay = document.getElementById("status");
+  const buttonDisplay = document.getElementById("button")
+  statusDisplay.textContent = (await checkOnlineStatus())
+    ? "Online"
+    : "OFFline";
+
+    
+});
+</script>
--- ProjectOnePHP/app/Views/test/online-offline.php
+++ ProjectOnePHP/app/Views/test/online-offline.php
@@ -6,18 +6,12 @@
     </div>
 </section>
 <script>
-/*  ********** Online / Offline Detection **********  */
-
-// Request a small image at an interval to determine status
-// ** Get a 1x1 pixel image here: http://www.1x1px.me/
-// ** Use this code with an HTML element with id="status"
-
 const checkOnlineStatus = async () => {
   try {
     const online = await fetch("https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/1pixel.png");
     return online.status >= 200 && online.status < 300; // either true or false
   } catch (err) {
-    return false; // definitely offline
+    return false; 
   }
 };
 
@@ -28,7 +22,6 @@ setInterval(async () => {
   statusDisplay.style.color = result ? "#005ea2" : "#d83933";
 }, 3000); // probably too often, try 30000 for every 30 seconds
 
-// forgot to include async load event listener in the video! 
 window.addEventListener("load", async (event) => {
   const statusDisplay = document.getElementById("status");
   statusDisplay.textContent = (await checkOnlineStatus())
