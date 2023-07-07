efe935ec136a9c4a23885ca327fa5ce338f4b892
Mike Stratton <134551281+mstratton1@users.noreply.github.com>
2023-07-07 05:20:53 -0400

    form links, csv crud demo

--- ProjectOnePHP/app/Views/boats/index.php
+++ ProjectOnePHP/app/Views/boats/index.php
@@ -2,7 +2,7 @@
     <div class="grid-container">
         <div class="mobile-lg:grid-col-4 margin-top-4 mobile-lg:margin-top-0">
             <h1 class="site-preview-heading margin-0">Vessels</h1>
-            <h3>Network Connection: <span id="status"></span></h3>
+            <!-- <h3>Network Connection: <span id="status"></span></h3> -->
             <ul class="usa-list usa-list--unstyled">
              <?php              
                 foreach($boats as $boat){
@@ -19,36 +19,36 @@
 </section>
 
 <script>
-const checkOnlineStatus = async () => {
-  try {
-    const online = await fetch("https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/1pixel.png");
-    return online.status >= 200 && online.status < 300; // either true or false
-  } catch (err) {
-    return false; 
-  }
-};
+// const checkOnlineStatus = async () => {
+//   try {
+//     const online = await fetch("https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/1pixel.png");
+//     return online.status >= 200 && online.status < 300; // either true or false
+//   } catch (err) {
+//     return false; 
+//   }
+// };
 
-setInterval(async () => {
-  const result = await checkOnlineStatus();
-  const statusDisplay = document.getElementById("status");
-  const btn1Display = document.getElementById("btn1")
-  const btn2Display = document.getElementById("btn2")
-  const btn3Display = document.getElementById("btn3")
-  statusDisplay.textContent = result ? "Online" : "Offline";
-  statusDisplay.style.color = result ? "#005ea2" : "#d83933";
-  btn1Display.style.display = result ? "" : "none";
-  btn2Display.style.display = result ? "" : "none";
-  btn3Display.style.display = result ? "" : "none";
+// setInterval(async () => {
+//   const result = await checkOnlineStatus();
+//   const statusDisplay = document.getElementById("status");
+//   const btn1Display = document.getElementById("btn1")
+//   const btn2Display = document.getElementById("btn2")
+//   const btn3Display = document.getElementById("btn3")
+//   statusDisplay.textContent = result ? "Online" : "Offline";
+//   statusDisplay.style.color = result ? "#005ea2" : "#d83933";
+//   btn1Display.style.display = result ? "" : "none";
+//   btn2Display.style.display = result ? "" : "none";
+//   btn3Display.style.display = result ? "" : "none";
   
-}, 3000); // probably too often, try 30000 for every 30 seconds
+// }, 3000); // probably too often, try 30000 for every 30 seconds
 
-window.addEventListener("load", async (event) => {
-  const statusDisplay = document.getElementById("status");
-  const buttonDisplay = document.getElementById("button")
-  statusDisplay.textContent = (await checkOnlineStatus())
-    ? "Online"
-    : "OFFline";
+// window.addEventListener("load", async (event) => {
+//   const statusDisplay = document.getElementById("status");
+//   const buttonDisplay = document.getElementById("button")
+//   statusDisplay.textContent = (await checkOnlineStatus())
+//     ? "Online"
+//     : "OFFline";
 
     
-});
+// });
 </script>
--- ProjectOnePHP/app/Views/test/form-links.php
+++ ProjectOnePHP/app/Views/test/form-links.php
@@ -15,7 +15,7 @@
                 <!-- <li><a href="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/index.php/test/log_catch" target="_blank">Log Catch</a></li> -->
                 <li><a href="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/index.php/test/GPS" target="_blank">GPS - Location</a></li>
                 <li><a href="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/index.php/test/end_haul" target="_blank">End Haul - Autofill</a></li>
-                <li><a href="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/index.php/test/online_offline" target="_blank">Online Test</a></li>
+                <!-- <li><a href="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/index.php/test/online_offline" target="_blank">Online Test</a></li> -->
                 <li><a href="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/index.php/BoatController/index" target="_blank">CSV CRUD Demo</a></li>
                 <li><a href="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/index.php/TripController/create" target="_blank">IndexedDB Autoincrement</a></li>
                 <!-- <li><a href="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/demo/grok_erd.pdf" target="_blank">Entity Relationship Diagram</a></li>
