a2be683f0001e7723716be253a46417f42676c60
Mike Stratton <134551281+mstratton1@users.noreply.github.com>
2023-07-07 14:32:28 -0400

    js in footer

--- ProjectOnePHP/app/Views/home2/includes/footer.php
+++ ProjectOnePHP/app/Views/home2/includes/footer.php
@@ -1,4 +1,17 @@
     </script>
     <script src="<?php echo base_url('/assets/uswds/uswds/js/uswds.min.js'); ?>"></script>
-    </body>
+    <script>
+        window.addEventListener("offline", (event) => {
+            console.log("The network connection has been lost.");
+            document.getElementById("offline").style.display = "block";
+            document.getElementById("online").style.display = "none";
+        });
+
+        window.addEventListener("online", (event) => {
+            console.log("You are now connected to the network.");
+            document.getElementById("offline").style.display = "none";
+            document.getElementById("online").style.display = "block";
+        });
+    </script>    
+</body>
 </html>
