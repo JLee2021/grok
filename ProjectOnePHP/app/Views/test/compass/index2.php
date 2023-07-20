7e3d83720a747294cf322f858503ca02566251c7
Mike Stratton <134551281+mstratton1@users.noreply.github.com>
2023-07-20 02:10:22 -0400

    USWDS demo pages formatting, line endings

--- ProjectOnePHP/app/Views/test/compass/index2.php
+++ ProjectOnePHP/app/Views/test/compass/index2.php
@@ -1,18 +1,10 @@
   <a class="usa-skipnav" href="#main-content">Skip to main content</a>
-  <section
-    class="usa-banner"
-    aria-label="Official website of the United States government"
-  >
+  <section class="usa-banner" aria-label="Official website of the United States government">
     <div class="usa-accordion">
       <header class="usa-banner__header">
         <div class="usa-banner__inner">
           <div class="grid-col-auto">
-            <img
-              aria-hidden="true"
-              class="usa-banner__header-flag"
-              src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/us_flag_small.png"
-              alt=""
-            />
+            <img aria-hidden="true" class="usa-banner__header-flag" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/us_flag_small.png" alt="" />
           </div>
           <div class="grid-col-fill tablet:grid-col-auto" aria-hidden="true">
             <p class="usa-banner__header-text">
@@ -20,29 +12,15 @@
             </p>
             <p class="usa-banner__header-action">Here’s how you know</p>
           </div>
-          <button
-            type="button"
-            class="usa-accordion__button usa-banner__button"
-            aria-expanded="false"
-            aria-controls="gov-banner-default"
-          >
+          <button type="button" class="usa-accordion__button usa-banner__button" aria-expanded="false" aria-controls="gov-banner-default">
             <span class="usa-banner__button-text">Here’s how you know</span>
           </button>
         </div>
       </header>
-      <div
-        class="usa-banner__content usa-accordion__content"
-        id="gov-banner-default"
-      >
+      <div class="usa-banner__content usa-accordion__content" id="gov-banner-default">
         <div class="grid-row grid-gap-lg">
           <div class="usa-banner__guidance tablet:grid-col-6">
-            <img
-              class="usa-banner__icon usa-media-block__img"
-              src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/icon-dot-gov.svg"
-              role="img"
-              alt=""
-              aria-hidden="true"
-            />
+            <img class="usa-banner__icon usa-media-block__img" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/icon-dot-gov.svg" role="img" alt="" aria-hidden="true" />
             <div class="usa-media-block__body">
               <p>
                 <strong>Official websites use .gov</strong><br />A
@@ -52,39 +30,18 @@
             </div>
           </div>
           <div class="usa-banner__guidance tablet:grid-col-6">
-            <img
-              class="usa-banner__icon usa-media-block__img"
-              src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/icon-https.svg"
-              role="img"
-              alt=""
-              aria-hidden="true"
-            />
+            <img class="usa-banner__icon usa-media-block__img" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/icon-https.svg" role="img" alt="" aria-hidden="true" />
             <div class="usa-media-block__body">
               <p>
                 <strong>Secure .gov websites use HTTPS</strong><br />A
                 <strong>lock</strong> (
-                <span class="icon-lock"
-                  ><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="52"
-                    height="64"
-                    viewBox="0 0 52 64"
-                    class="usa-banner__lock-image"
-                    role="img"
-                    aria-labelledby="banner-lock-description"
-                    focusable="false"
-                  >
+                <span class="icon-lock"><svg xmlns="http://www.w3.org/2000/svg" width="52" height="64" viewBox="0 0 52 64" class="usa-banner__lock-image" role="img" aria-labelledby="banner-lock-description" focusable="false">
                     <title id="banner-lock-title">Lock</title>
                     <desc id="banner-lock-description">
                       Locked padlock icon
                     </desc>
-                    <path
-                      fill="#000000"
-                      fill-rule="evenodd"
-                      d="M26 0c10.493 0 19 8.507 19 19v9h3a4 4 0 0 1 4 4v28a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V32a4 4 0 0 1 4-4h3v-9C7 8.507 15.507 0 26 0zm0 8c-5.979 0-10.843 4.77-10.996 10.712L15 19v9h22v-9c0-6.075-4.925-11-11-11z"
-                    />
-                  </svg> </span
-                >) or <strong>https://</strong> means you’ve safely connected to
+                    <path fill="#000000" fill-rule="evenodd" d="M26 0c10.493 0 19 8.507 19 19v9h3a4 4 0 0 1 4 4v28a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V32a4 4 0 0 1 4-4h3v-9C7 8.507 15.507 0 26 0zm0 8c-5.979 0-10.843 4.77-10.996 10.712L15 19v9h22v-9c0-6.075-4.925-11-11-11z" />
+                  </svg> </span>) or <strong>https://</strong> means you’ve safely connected to
                 the .gov website. Share sensitive information only on official,
                 secure websites.
               </p>
@@ -99,9 +56,7 @@
     <div class="usa-nav-container">
       <div class="usa-navbar">
         <div class="usa-logo">
-          <em class="usa-logo__text"
-            >COMPASS</em
-          >
+          <em class="usa-logo__text">COMPASS</em>
         </div>
       </div>
     </div>
@@ -149,11 +104,7 @@
       <div class="grid-container">
         <div class="usa-graphic-list__row grid-row grid-gap">
           <div class="usa-media-block tablet:grid-col">
-            <img
-              class="usa-media-block__img"
-              src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-124.png"
-              alt="Alt text"
-            />
+            <img class="usa-media-block__img" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-124.png" alt="Alt text" />
             <div class="usa-media-block__body">
               <h2 class="usa-graphic-list__heading">
                 Graphic headings can vary.
@@ -166,11 +117,7 @@
             </div>
           </div>
           <div class="usa-media-block tablet:grid-col">
-            <img
-              class="usa-media-block__img"
-              src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-124.png"
-              alt="Alt text"
-            />
+            <img class="usa-media-block__img" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-124.png" alt="Alt text" />
             <div class="usa-media-block__body">
               <h2 class="usa-graphic-list__heading">
                 Stick to 6 or fewer words.
@@ -185,11 +132,7 @@
         </div>
         <div class="usa-graphic-list__row grid-row grid-gap">
           <div class="usa-media-block tablet:grid-col">
-            <img
-              class="usa-media-block__img"
-              src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-124.png"
-              alt="Alt text"
-            />
+            <img class="usa-media-block__img" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-124.png" alt="Alt text" />
             <div class="usa-media-block__body">
               <h2 class="usa-graphic-list__heading">
                 Never highlight anything without a goal.
@@ -202,11 +145,7 @@
             </div>
           </div>
           <div class="usa-media-block tablet:grid-col">
-            <img
-              class="usa-media-block__img"
-              src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-124.png"
-              alt="Alt text"
-            />
+            <img class="usa-media-block__img" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-124.png" alt="Alt text" />
             <div class="usa-media-block__body">
               <h2 class="usa-graphic-list__heading">Could also have 2 or 6.</h2>
               <p>
@@ -240,49 +179,33 @@
     <div class="usa-footer__primary-section">
       <nav class="usa-footer__nav" aria-label="Footer navigation">
         <ul class="grid-row grid-gap">
-          <li
-            class="
+          <li class="
               mobile-lg:grid-col-4
               desktop:grid-col-auto
               usa-footer__primary-content
-            "
-          >
-            <a class="usa-footer__primary-link" href="javascript:void(0);"
-              >&lt;Primary link&gt;</a
-            >
+            ">
+            <a class="usa-footer__primary-link" href="javascript:void(0);">&lt;Primary link&gt;</a>
           </li>
-          <li
-            class="
+          <li class="
               mobile-lg:grid-col-4
               desktop:grid-col-auto
               usa-footer__primary-content
-            "
-          >
-            <a class="usa-footer__primary-link" href="javascript:void(0);"
-              >&lt;Primary link&gt;</a
-            >
+            ">
+            <a class="usa-footer__primary-link" href="javascript:void(0);">&lt;Primary link&gt;</a>
           </li>
-          <li
-            class="
+          <li class="
               mobile-lg:grid-col-4
               desktop:grid-col-auto
               usa-footer__primary-content
-            "
-          >
-            <a class="usa-footer__primary-link" href="javascript:void(0);"
-              >&lt;Primary link&gt;</a
-            >
+            ">
+            <a class="usa-footer__primary-link" href="javascript:void(0);">&lt;Primary link&gt;</a>
           </li>
-          <li
-            class="
+          <li class="
               mobile-lg:grid-col-4
               desktop:grid-col-auto
               usa-footer__primary-content
-            "
-          >
-            <a class="usa-footer__primary-link" href="javascript:void(0);"
-              >&lt;Primary link&gt;</a
-            >
+            ">
+            <a class="usa-footer__primary-link" href="javascript:void(0);">&lt;Primary link&gt;</a>
           </li>
         </ul>
       </nav>
@@ -290,19 +213,13 @@
     <div class="usa-footer__secondary-section">
       <div class="grid-container">
         <div class="grid-row grid-gap">
-          <div
-            class="
+          <div class="
               usa-footer__logo
               grid-row
               mobile-lg:grid-col-6 mobile-lg:grid-gap-2
-            "
-          >
+            ">
             <div class="mobile-lg:grid-col-auto">
-              <img
-                class="usa-footer__logo-img"
-                src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/logo-img.png"
-                alt=""
-              />
+              <img class="usa-footer__logo-img" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/logo-img.png" alt="" />
             </div>
             <div class="mobile-lg:grid-col-auto">
               <p class="usa-footer__logo-heading">&lt;Name of Agency&gt;</p>
@@ -311,44 +228,19 @@
           <div class="usa-footer__contact-links mobile-lg:grid-col-6">
             <div class="usa-footer__social-links grid-row grid-gap-1">
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                  ><img
-                    class="usa-social-link__icon"
-                    src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/facebook.svg"
-                    alt="Facebook"
-                /></a>
+                <a class="usa-social-link" href="javascript:void(0);"><img class="usa-social-link__icon" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/facebook.svg" alt="Facebook" /></a>
               </div>
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                  ><img
-                    class="usa-social-link__icon"
-                    src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/twitter.svg"
-                    alt="Twitter"
-                /></a>
+                <a class="usa-social-link" href="javascript:void(0);"><img class="usa-social-link__icon" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/twitter.svg" alt="Twitter" /></a>
               </div>
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                  ><img
-                    class="usa-social-link__icon"
-                    src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/youtube.svg"
-                    alt="YouTube"
-                /></a>
+                <a class="usa-social-link" href="javascript:void(0);"><img class="usa-social-link__icon" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/youtube.svg" alt="YouTube" /></a>
               </div>
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                  ><img
-                    class="usa-social-link__icon"
-                    src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/instagram.svg"
-                    alt="Instagram"
-                /></a>
+                <a class="usa-social-link" href="javascript:void(0);"><img class="usa-social-link__icon" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/instagram.svg" alt="Instagram" /></a>
               </div>
               <div class="grid-col-auto">
-                <a class="usa-social-link" href="javascript:void(0);"
-                  ><img
-                    class="usa-social-link__icon"
-                    src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/rss_feed.svg"
-                    alt="RSS"
-                /></a>
+                <a class="usa-social-link" href="javascript:void(0);"><img class="usa-social-link__icon" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/usa-icons/rss_feed.svg" alt="RSS" /></a>
               </div>
             </div>
             <p class="usa-footer__contact-heading">
@@ -370,24 +262,12 @@
     </div>
   </footer>
   <div class="usa-identifier">
-    <section
-      class="usa-identifier__section usa-identifier__section--masthead"
-      aria-label="Agency identifier"
-    >
+    <section class="usa-identifier__section usa-identifier__section--masthead" aria-label="Agency identifier">
       <div class="usa-identifier__container">
         <div class="usa-identifier__logos">
-          <a href="javascript:void(0)" class="usa-identifier__logo"
-            ><img
-              class="usa-identifier__logo-img"
-              src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-gray-20.svg"
-              alt="&lt;Parent agency&gt; logo"
-              role="img"
-          /></a>
+          <a href="javascript:void(0)" class="usa-identifier__logo"><img class="usa-identifier__logo-img" src="https://nefsctest.nmfs.local/grok/html/ProjectOnePHP/public/assets/img/circle-gray-20.svg" alt="&lt;Parent agency&gt; logo" role="img" /></a>
         </div>
-        <section
-          class="usa-identifier__identity"
-          aria-label="Agency description"
-        >
+        <section class="usa-identifier__identity" aria-label="Agency description">
           <p class="usa-identifier__identity-domain">domain.gov</p>
           <p class="usa-identifier__identity-disclaimer">
             An official website of the <a href="">&lt;Parent agency&gt;</a>
@@ -395,56 +275,34 @@
         </section>
       </div>
     </section>
-    <nav
-      class="usa-identifier__section usa-identifier__section--required-links"
-      aria-label="Important links"
-    >
+    <nav class="usa-identifier__section usa-identifier__section--required-links" aria-label="Important links">
       <div class="usa-identifier__container">
         <ul class="usa-identifier__required-links-list">
           <li class="usa-identifier__required-links-item">
-            <a
-              href="javascript:void(0)"
-              class="usa-identifier__required-link usa-link"
-              >About &lt;Parent shortname&gt;</a
-            >
+            <a href="javascript:void(0)" class="usa-identifier__required-link usa-link">About &lt;Parent shortname&gt;</a>
           </li>
           <li class="usa-identifier__required-links-item">
-            <a href="" class="usa-identifier__required-link usa-link"
-              >Accessibility support</a
-            >
+            <a href="" class="usa-identifier__required-link usa-link">Accessibility support</a>
           </li>
           <li class="usa-identifier__required-links-item">
-            <a href="" class="usa-identifier__required-link usa-link"
-              >FOIA requests</a
-            >
+            <a href="" class="usa-identifier__required-link usa-link">FOIA requests</a>
           </li>
           <li class="usa-identifier__required-links-item">
-            <a href="" class="usa-identifier__required-link usa-link"
-              >No FEAR Act data</a
-            >
+            <a href="" class="usa-identifier__required-link usa-link">No FEAR Act data</a>
           </li>
           <li class="usa-identifier__required-links-item">
-            <a href="" class="usa-identifier__required-link usa-link"
-              >Office of the Inspector General</a
-            >
+            <a href="" class="usa-identifier__required-link usa-link">Office of the Inspector General</a>
           </li>
           <li class="usa-identifier__required-links-item">
-            <a href="" class="usa-identifier__required-link usa-link"
-              >Performance reports</a
-            >
+            <a href="" class="usa-identifier__required-link usa-link">Performance reports</a>
           </li>
           <li class="usa-identifier__required-links-item">
-            <a href="" class="usa-identifier__required-link usa-link"
-              >Privacy policy</a
-            >
+            <a href="" class="usa-identifier__required-link usa-link">Privacy policy</a>
           </li>
         </ul>
       </div>
     </nav>
-    <section
-      class="usa-identifier__section usa-identifier__section--usagov"
-      aria-label="U.S. government information and services"
-    >
+    <section class="usa-identifier__section usa-identifier__section--usagov" aria-label="U.S. government information and services">
       <div class="usa-identifier__container">
         <div class="usa-identifier__usagov-description">
           Looking for U.S. government information and services?
@@ -453,4 +311,3 @@
       </div>
     </section>
   </div>
-
